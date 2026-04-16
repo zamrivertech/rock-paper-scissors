deaa19db82b012003ee49dd03a77355166002d61
zamrivertech <mohamadtasul8000@gmail.com>
2026-04-16 10:05:00 +0200

    Test getComputerChoice() and it works, returns selected choice

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-console.log("hello world");
-
 const choices = ["rock","paper","scissors"];
-
-
-
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)];
-    // Always choosing scissors and paper, not rock or 0
+    //it does not reach 3, only 0, 1 and 2
 }
 
-console.log(Math.floor(Math.random()*3));
+console.log(getComputerChoice());
