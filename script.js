3070d4583974d50b5bf3a26818e4d3268210705d
zamrivertech <mohamadtasul8000@gmail.com>
2026-04-16 09:55:50 +0200

    Add random logic to choices array returned by getComputerChoice

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ console.log("hello world");
 const choices = ["rock","paper","scissors"];
 
 
-function getComputerChoice() {
 
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random()*3)+1];
+    // Always choosing scissors and paper, not rock or 0
 }
 
-console.log(choices[0]);
+console.log(Math.floor(Math.random()*2)+1);
