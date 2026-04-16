5e6080e63aa1a677d461fecb1fce47467cf2f32a
zamrivertech <mohamadtasul8000@gmail.com>
2026-04-16 11:07:34 +0200

    Create empty playround function

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function getHumanChoice(choice) {
     return choices[choice];
     //it does not reach 3, only 0, 1 and 2
 }
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
 console.log("Hello, Dear User, please enter one of the following below");
 console.log("Rock: 0, Paper: 1, Scissors: 2");
 console.log(getHumanChoice(prompt("Your choice is:")));
