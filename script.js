f8c1fd58fdc782811d14efd096567f9e3c7e53a0
zamrivertech <mohamadtasul8000@gmail.com>
2026-04-16 13:19:09 +0200

    Add logic to playRound and test and it's working

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const choices = ["rock","paper","scissors"];
 
 let humanScore = 0;
 let computerScore = 0;
+let currentHumanChoice = "";
+let currentComputerChoice = "";
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random()*3)];
+    currentComputerChoice = choices[Math.floor(Math.random()*3)];
+    return currentComputerChoice;
     //it does not reach 3, only 0, 1 and 2
 }
 
@@ -13,6 +16,7 @@ function getHumanChoice(choice) {
 
     for (let i = 0; i < choices.length; i++) {
         if (choice == choices[i]) {
+            currentHumanChoice = choice;
             return choice;
         }
     }
@@ -23,9 +27,58 @@ function getHumanChoice(choice) {
 
 function playRound(humanChoice, computerChoice) {
 
+    humanChoice = currentHumanChoice;
+    computerChoice = currentComputerChoice;
+    //make game logic
+
+    //HUMAN
+
+    //scissors beats paper
+    if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        return "You Won!";
+    }
+
+    //paper beats rock
+    if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++;
+        return "You Won!";
+    }
+
+    //rock beats scissors
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++;
+        return "You Won!";
+    }    
+
+        //COMPUTER
+
+    //scissors beats paper
+    if (computerChoice == "scissors" && humanChoice == "paper") {
+        computerScore++;
+        return "You Lost!";
+    }
+
+    //paper beats rock
+    if (computerChoice == "paper" && humanChoice == "rock") {
+        computerScore++;
+        return "You Lost!";
+    }
+
+    //rock beats scissors
+    if (computerChoice == "rock" && humanChoice == "scissors") {
+        computerScore++;
+        return "You Lost!";
+    } 
+
+        return "It's a Tie";
+
 }
 
 console.log("Hello, Dear User, please enter one of the following below");
 console.log("Rock, Paper or Scissors");
 console.log("You chose: ");
-console.log(getHumanChoice(prompt("Your choice is:")));
+console.log(getHumanChoice(prompt("Your choice is:", "paper")));
+console.log("Computer choice is:");
+console.log(getComputerChoice());
+console.log(playRound(currentComputerChoice, currentHumanChoice));
