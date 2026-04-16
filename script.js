1395965fc920d83f55ab091bfecaf64beb409541
zamrivertech <mohamadtasul8000@gmail.com>
2026-04-16 10:17:06 +0200

    Create getHumanChoice and test choice, and OK

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ function getComputerChoice() {
     //it does not reach 3, only 0, 1 and 2
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(choice) {
+    return choices[choice];
+    //it does not reach 3, only 0, 1 and 2
+}
+console.log("Hello, Dear User, please enter one of the following below");
+console.log("Rock: 0, Paper: 1, Scissors: 2");
+console.log(getHumanChoice(prompt("Your choice is:")));
