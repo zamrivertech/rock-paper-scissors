1213598d5455ff9b667a7cb08ce155ff8ff69640
zamrivertech <mohamadtasul8000@gmail.com>
2026-04-16 10:03:27 +0200

    Fix random by multiplying with 3 only

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const choices = ["rock","paper","scissors"];
 
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random()*3)+1];
+    return choices[Math.floor(Math.random()*3)];
     // Always choosing scissors and paper, not rock or 0
 }
 
-console.log(Math.floor(Math.random()*2)+1);
+console.log(Math.floor(Math.random()*3));
