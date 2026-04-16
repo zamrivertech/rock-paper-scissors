76645c1638cd3c45a8d979ec6279836a56dcf2d1
zamrivertech <mohamadtasul8000@gmail.com>
2026-04-16 11:05:28 +0200

    Create human and computer score variables

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const choices = ["rock","paper","scissors"];
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)];
     //it does not reach 3, only 0, 1 and 2
